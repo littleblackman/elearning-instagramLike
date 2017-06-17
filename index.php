d90067118a0d196852082a4e809937658e4bea84
littleblackman <sandyrazafitrimo@gmail.com>
2017-06-17 16:00:59 +0200

    first version html integration

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>my Insta</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" >
+
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav-item">
+                <a href="#">
+                    <i class="material-icons">camera_alt</i>
+                    <h1>Instagram Like</h1>
+                </a>
+                <h1 style="line-height: 1em"></h1>
+            </div>
+            <div class="nav-item">
+                <input id="input-search" type="text" placeholder="Rechercher"/>
+            </div>
+            <div class="nav-item end-row">
+                <a href="#">
+                    <i class="material-icons">star_border</i>
+                </a>
+                <a href="#">
+                    <i class="material-icons">person_outline</i>
+                </a>
+            </div>
+        </nav>
+    </header>
+
+    <div id="container">
+        <section id="flux">
+            <figure class="content-image">
+                <figcaption>
+                    <span class="author">
+                        eyesofchildrenaroundtheworld
+                    </span>
+                    Photo by @miweb_photography
+                </figcaption>
+                <img src="insta1.jpg"/>
+            </figure>
+
+            <figure>
+                <figcaption>
+                    <span class="author">
+                        eyesofchildrenaroundtheworld
+                    </span>
+                    Photo by @geosmin_photography
+                </figcaption>
+                <img src="insta2.jpg"/>
+            </figure>
+
+        </section>
+    </div>
+
+</body>
+</html>
