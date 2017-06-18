ce89e3e3a3b683f6346ad7e81c7af313b7faedf7
littleblackman <sandyrazafitrimo@gmail.com>
2017-06-18 21:13:22 +0200

    first version html integration

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="style.css"/>
     <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" >
-
 </head>
 <body>
     <header>
