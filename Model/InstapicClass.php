c7cd758aee271d53534085331b634f7266736370
littleblackman <sandyrazafitrimo@gmail.com>
2017-06-25 19:17:41 +0200

    add the class instapic

---
+++ Model/InstapicClass.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class Instapic
+ * Class names MUST be declared in StudlyCaps.
+ *
+ */
+class Instapic
+{
+    /**
+     * @var string
+     */
+    private $author;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+}
